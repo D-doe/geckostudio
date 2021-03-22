9b9e367f906578ff001287961578a6d32f948261
unknown <geckouidesign@gmail.com>
2021-03-23 02:29:18 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//js doc
+  $(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
